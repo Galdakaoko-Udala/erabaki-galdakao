e1af4040e457233fcb480b17b8ab8e7265371c35
paranoix <jleiva@alabs.org>
2022-05-02 13:50:59 +0200

    actualización de repo 02/05/2022

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
@@ -86,7 +86,7 @@ Rails.application.configure do
     :password       => Rails.application.secrets.smtp_password,
     :domain         => Rails.application.secrets.smtp_domain,
     :enable_starttls_auto => Rails.application.secrets.smtp_starttls_auto,
-    :openssl_verify_mode => 'none'
+    :openssl_verify_mode => 'none',
   }
 
   # Use a different logger for distributed setups.
---
+++ config/initializers/main_navigation.rb
@@ -0,0 +1,5 @@
+Decidim.menu :menu do |menu|
+  menu.item I18n.t("menu.portal"), "https://galdakao.egoitzaelektronikoa.eus/transparency",
+            position: 4,
+            active: :inclusive
+end
