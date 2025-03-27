2cf49804da9459dad65b0873fd2169ec28168f2a
Ivan Vergés <ivan@pokecode.net>
2025-03-27 17:58:40 +0100

    build fix

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [ -f .env ]; then
+  source .env
+fi
+
 if [ -z "$DECIDIM_ENV" ]; then
   echo "Please set the DECIDIM_ENV environment variable"
+  echo "You can define it in a .env file and it will be automatically loaded"
   exit 1
 fi
 
