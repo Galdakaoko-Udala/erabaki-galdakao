1b67ac365de67e045b3538bd4bfbfc3c6951b37e
Ivan Vergés <ivan@pokecode.net>
2025-03-27 16:25:02 +0100

    remove custom menu

--- config/initializers/main_navigation.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Decidim.menu :menu do |menu|
-  menu.item I18n.t("menu.portal"), "https://gardentasuna.galdakao.eus",
-            position: 4,
-            active: :inclusive
-end
