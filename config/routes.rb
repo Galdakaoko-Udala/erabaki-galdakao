e28960a6fa705366029e55be2dd94b9de23d2c0f
Ivan Vergés <ivan@pokecode.net>
2025-05-06 12:14:22 +0200

    add file authorization handler

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   end
 
   mount Decidim::Core::Engine => "/"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Decidim::FileAuthorizationHandler::AdminEngine => "/admin"
 end
