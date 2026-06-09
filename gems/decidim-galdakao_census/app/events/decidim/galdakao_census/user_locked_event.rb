83e6054ae8412c08e21c2bc38dbdd37b7dbcf033
root <root@soap-galda-decidim31.lxd>
2026-06-09 09:53:10 +0000

    fix: corregir helper de rutas en UserLockedEvent para email al admin
    Cambiado EngineRouter de 'decidim_admin' a 'decidim_admin_galdakao_census'
    para que galdakao_blocked_users_path se resuelva correctamente en el
    contexto del mailer. El job fallaba con NoMethodError en producción.

--- gems/decidim-galdakao_census/app/events/decidim/galdakao_census/user_locked_event.rb
+++ gems/decidim-galdakao_census/app/events/decidim/galdakao_census/user_locked_event.rb
@@ -1,38 +1,29 @@
 # frozen_string_literal: true
-
 module Decidim
   module GaldakaoCensus
     class UserLockedEvent < Decidim::Events::SimpleEvent
       include Rails.application.routes.mounted_helpers
-
       def i18n_scope
         "decidim.events.galdakao_census.user_locked"
       end
-
       def resource_path
         nil
       end
-
       def resource_url
         nil
       end
-
       def resource_title
         nil
       end
-
       def default_i18n_options
         super.merge({
-          blocked_users_path: decidim_admin.galdakao_blocked_users_path
+          blocked_users_path: decidim_admin_galdakao_census.galdakao_blocked_users_path
         })
       end
-
       private
-
-      def decidim_admin
-        @decidim_admin ||= Decidim::EngineRouter.new("decidim_admin", { host: organization.host })
+      def decidim_admin_galdakao_census
+        @decidim_admin_galdakao_census ||= Decidim::EngineRouter.new("decidim_admin_galdakao_census", { host: organization.host })
       end
-
       def organization
         resource.organization
       end
