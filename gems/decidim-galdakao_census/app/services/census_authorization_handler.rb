a72e09ce221868c0134236c43d0c7749d5e648be
Patuksa <Vettelpatuksa@gmail.com>
2026-06-15 10:22:27 +0200

    Update, removed old authentication code, to new service

--- gems/decidim-galdakao_census/app/services/census_authorization_handler.rb
+++ gems/decidim-galdakao_census/app/services/census_authorization_handler.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 require "digest/md5"
-require "faraday"
-require "nokogiri"
+
 
 class CensusAuthorizationHandler < Decidim::AuthorizationHandler
   attribute :document_number, String
@@ -25,10 +24,6 @@ class CensusAuthorizationHandler < Decidim::AuthorizationHandler
 
   def unique_id
     Digest::MD5.hexdigest("#{document_number&.upcase}-#{Rails.application.secret_key_base}")
-  end
-
-  def slim_response
-    response&.search("Body")&.children
   end
 
   private
@@ -77,45 +72,13 @@ class CensusAuthorizationHandler < Decidim::AuthorizationHandler
 
   def response
     return @response if defined?(@response)
-    Rails.logger.info "[Galdakao-Census] ENV CENSUS_URL directo: #{ENV['CENSUS_URL'].inspect}"
-    census_url = ENV["CENSUS_URL"]
-    Rails.logger.info "[Galdakao-Census] Census URL: #{census_url.inspect}"
-    if census_url.blank?
-      Rails.logger.error "[Galdakao-Census] CENSUS_URL no configurado"
-      return @response = nil
-    end
-    begin
-      faraday_response = Faraday.post(census_url) do |request|
-        request.headers["Content-Type"] = "text/xml; charset=UTF-8"
-        request.body = request_body
-      end
-      Rails.logger.info "[Galdakao-Census] SOAP status: #{faraday_response.status}"
-      Rails.logger.info "[Galdakao-Census] SOAP body: #{faraday_response.body}"
-      if faraday_response.status.to_i != 200
-        Rails.logger.error "[Galdakao-Census] SOAP error HTTP #{faraday_response.status}"
-        return @response = nil
-      end
-      xml = Nokogiri::XML(faraday_response.body)
-      xml.remove_namespaces!
-      @response = xml
-    rescue Faraday::Error => e
-      Rails.logger.error "[Galdakao-Census] SOAP connection error: #{e.message}"
-      @response = nil
-    end
-  end
 
-  def request_body
-    <<~XML
-      <?xml version="1.0" encoding="UTF-8"?>
-      <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
-                    xmlns:tns="http://example.com/soap">
-        <soap:Body>
-          <tns:autenticar>
-            <tns:dni>#{document_number&.upcase}</tns:dni>
-            <tns:fecha_nacimiento>#{sanitized_date_of_birth}</tns:fecha_nacimiento>
-          </tns:autenticar>
-        </soap:Body>
-      </soap:Envelope>
+    ws = GaldakaoWebservice.new("autenticar")
+    ws.body = <<~XML
+      <tns:dni>#{document_number&.upcase}</tns:dni>
+      <tns:fecha_nacimiento>#{sanitized_date_of_birth}</tns:fecha_nacimiento>
     XML
+
+    @response = ws.response
   end
 end
