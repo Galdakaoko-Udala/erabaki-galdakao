43ae005bd7f0562efb23cf5261980be2aad88d93
Patuksa <Vettelpatuksa@gmail.com>
2026-07-06 13:32:33 +0200

    spec models

--- gems/decidim-galdakao_census/spec/models/galdakao_zone_spec.rb
+++ gems/decidim-galdakao_census/spec/models/galdakao_zone_spec.rb
@@ -2,21 +2,56 @@
 
 require "rails_helper"
 
-RSpec.describe GaldakaoZone, type: :model do
-  subject(:zone) { build(:galdakao_zone) }
+describe GaldakaoZone do
+  describe "associations" do
+    it "belongs to an organization" do
+      zone = create(:galdakao_zone)
 
-  it { is_expected.to be_valid }
+      expect(zone.organization).to be_a(Decidim::Organization)
+    end
 
-  describe "validations" do
-    it "requires a name" do
-      zone.name = nil
-      expect(zone).not_to be_valid
+    it "has many zone_streets" do
+      zone = create(:galdakao_zone)
+      zone_street_1 = create(:galdakao_zone_street, zone:)
+      zone_street_2 = create(:galdakao_zone_street, zone:)
+
+      expect(zone.zone_streets).to contain_exactly(zone_street_1, zone_street_2)
+    end
+
+    it "destroys associated zone_streets when destroyed" do
+      zone = create(:galdakao_zone)
+      create(:galdakao_zone_street, zone:)
+
+      expect { zone.destroy }.to change(GaldakaoZoneStreet, :count).by(-1)
+    end
+
+    it "has many streets through zone_streets" do
+      zone = create(:galdakao_zone)
+      street = create(:galdakao_street, organization: zone.organization)
+      create(:galdakao_zone_street, zone:, street:)
+
+      expect(zone.streets).to contain_exactly(street)
+    end
+
+    it "does not list a street that is not linked through a zone_street" do
+      zone = create(:galdakao_zone)
+      create(:galdakao_street, organization: zone.organization)
+
+      expect(zone.streets).to be_empty
     end
   end
 
-  describe "associations" do
-    it { is_expected.to belong_to(:organization).class_name("Decidim::Organization") }
-    it { is_expected.to have_many(:zone_streets).class_name("GaldakaoZoneStreet").dependent(:destroy) }
-    it { is_expected.to have_many(:streets).class_name("GaldakaoStreet").through(:zone_streets) }
+  describe "validations" do
+    it "is valid with a name" do
+      zone = build(:galdakao_zone, name: "Zona Centro")
+
+      expect(zone).to be_valid
+    end
+
+    it "is not valid without a name" do
+      zone = build(:galdakao_zone, name: nil)
+
+      expect(zone).not_to be_valid
+    end
   end
 end
--- gems/decidim-galdakao_census/spec/models/galdakao_zone_street_spec.rb
+++ gems/decidim-galdakao_census/spec/models/galdakao_zone_street_spec.rb
@@ -2,51 +2,23 @@
 
 require "rails_helper"
 
-RSpec.describe GaldakaoZoneStreet, type: :model do
-  subject(:zone_street) { build(:galdakao_zone_street) }
-
-  it { is_expected.to be_valid }
-
+describe GaldakaoZoneStreet do
   describe "associations" do
-    it { is_expected.to belong_to(:zone).class_name("GaldakaoZone") }
-    it { is_expected.to belong_to(:street).class_name("GaldakaoStreet") }
-  end
+    it "belongs to a zone" do
+      zone_street = create(:galdakao_zone_street)
 
-  describe "validations" do
-    it { is_expected.to validate_presence_of(:numbers_constraint) }
-
-    context "when numbers_constraint requires a range" do
-      subject(:zone_street) { build(:galdakao_zone_street, :only_range, numbers_range: nil) }
-
-      it "requires numbers_range to be present" do
-        expect(zone_street).not_to be_valid
-        expect(zone_street.errors[:numbers_range]).to be_present
-      end
+      expect(zone_street.zone).to be_a(GaldakaoZone)
     end
 
-    context "when numbers_constraint does not require a range" do
-      subject(:zone_street) { build(:galdakao_zone_street, numbers_constraint: :all_numbers, numbers_range: nil) }
+    it "belongs to a street" do
+      zone_street = create(:galdakao_zone_street)
 
-      it "does not require numbers_range" do
-        expect(zone_street).to be_valid
-      end
-    end
-
-    context "when numbers_range has a valid format" do
-      subject(:zone_street) { build(:galdakao_zone_street, :only_range, numbers_range: "1-10,15,20-25") }
-
-      it { is_expected.to be_valid }
-    end
-
-    context "when numbers_range has an invalid format" do
-      subject(:zone_street) { build(:galdakao_zone_street, :only_range, numbers_range: "abc") }
-
-      it { is_expected.not_to be_valid }
+      expect(zone_street.street).to be_a(GaldakaoStreet)
     end
   end
 
-  describe "enum numbers_constraint" do
-    it "defines the expected values" do
+  describe "numbers_constraint enum" do
+    it "exposes the expected constraint values" do
       expect(described_class.numbers_constraints).to eq(
         "all_numbers" => 0,
         "odd_numbers" => 1,
@@ -54,6 +26,94 @@ RSpec.describe GaldakaoZoneStreet, type: :model do
         "only_range" => 3,
         "except_range" => 4
       )
+    end
+  end
+
+  describe "validations" do
+    it "is not valid without a zone" do
+      zone_street = build(:galdakao_zone_street, zone: nil)
+
+      expect(zone_street).not_to be_valid
+    end
+
+    it "is not valid without a street" do
+      zone_street = build(:galdakao_zone_street, street: nil)
+
+      expect(zone_street).not_to be_valid
+    end
+
+    it "is not valid without a numbers_constraint" do
+      zone_street = build(:galdakao_zone_street, numbers_constraint: nil)
+
+      expect(zone_street).not_to be_valid
+    end
+
+    context "when numbers_constraint does not require a range" do
+      it "is valid without a numbers_range" do
+        zone_street = build(:galdakao_zone_street, numbers_constraint: :all_numbers, numbers_range: nil)
+
+        expect(zone_street).to be_valid
+      end
+    end
+
+    context "when numbers_constraint is only_range" do
+      it "is not valid without a numbers_range" do
+        zone_street = build(:galdakao_zone_street, numbers_constraint: :only_range, numbers_range: nil)
+
+        expect(zone_street).not_to be_valid
+      end
+
+      it "is valid with a numbers_range" do
+        zone_street = build(:galdakao_zone_street, numbers_constraint: :only_range, numbers_range: "1-10")
+
+        expect(zone_street).to be_valid
+      end
+    end
+
+    context "when numbers_constraint is except_range" do
+      it "is not valid without a numbers_range" do
+        zone_street = build(:galdakao_zone_street, numbers_constraint: :except_range, numbers_range: nil)
+
+        expect(zone_street).not_to be_valid
+      end
+    end
+
+    context "numbers_range format" do
+      it "is valid with a single number" do
+        zone_street = build(:galdakao_zone_street, numbers_constraint: :only_range, numbers_range: "4")
+
+        expect(zone_street).to be_valid
+      end
+
+      it "is valid with a simple range" do
+        zone_street = build(:galdakao_zone_street, numbers_constraint: :only_range, numbers_range: "1-10")
+
+        expect(zone_street).to be_valid
+      end
+
+      it "is valid with a comma separated list of numbers and ranges" do
+        zone_street = build(:galdakao_zone_street, numbers_constraint: :only_range, numbers_range: "1-4,7,10-12")
+
+        expect(zone_street).to be_valid
+      end
+
+      it "is not valid with letters" do
+        zone_street = build(:galdakao_zone_street, numbers_constraint: :only_range, numbers_range: "abc")
+
+        expect(zone_street).not_to be_valid
+      end
+
+      it "is not valid with a trailing separator" do
+        zone_street = build(:galdakao_zone_street, numbers_constraint: :only_range, numbers_range: "1-4,")
+
+        expect(zone_street).not_to be_valid
+      end
+
+      it "is not valid with a malformed range" do
+        zone_street = build(:galdakao_zone_street, numbers_constraint: :only_range, numbers_range: "1--4")
+
+        expect(zone_street).not_to be_valid
+      end
     end
   end
 end
